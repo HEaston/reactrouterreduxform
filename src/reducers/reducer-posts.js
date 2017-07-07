a7622a525b553999850ab17d2f91a30775695859
Easton, Hayley <heaston@netsuite.com>
2017-07-07 21:43:59 +0100

    Section 9, Lesson 144 complete

--- src/reducers/reducer-posts.js
+++ src/reducers/reducer-posts.js
@@ -1,8 +1,22 @@
 import _ from 'lodash';
 import { FETCH_POSTS } from '../actions'; // will import from the index file
+import { FETCH_POST } from '../actions';
 
 export default function(state = {}, action) {
   switch (action.type) {
+    case FETCH_POST:;
+      // ES5 version
+      //const post = action.payload.data;
+      //const newState = { ...state };
+      //newState[post.id] = post;
+      //return newState;
+
+      // ES6 cleaned version
+      return { ...state, [action.payload.data.id]: action.payload.data };
+      // [KEY]: VALUE
+      //
+      // ...state means take all the existing posts we have from the state
+      // object and put them into new object we're about to return
     case FETCH_POSTS:
       // console.log(action.payload.data);
       return _.mapKeys(action.payload.data, 'id');
