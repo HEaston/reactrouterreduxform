feaeae16ccd0429ca4d6517d65a798755bf7e466
Easton, Hayley <heaston@netsuite.com>
2017-07-07 15:08:38 +0100

    Section 9, Lesson 135 complete

--- src/components/posts-new.js
+++ src/components/posts-new.js
@@ -26,9 +26,9 @@ class PostsNew extends Component {
           label="Title"
         />
         <Field
-          name="tags"
+          name="categories"
           component={this.renderField}
-          label="Tags"
+          label="Categories"
         />
         <Field
           name="content"
@@ -40,9 +40,33 @@ class PostsNew extends Component {
   }
 }
 
+// helper function
+function validate(values) {
+  // console.log(values) -> { title: 'asdf', categories: 'asdf', content: 'asdf' }
+  const errors = {};
+
+  // Validate the inputs from 'values'
+  if (!values.title || values.title.length < 3) {
+    errors.title = "Enter a title that is at least 3 characters";
+  }
+  // can break out into multiple if statements if wanted
+  if (!values.categories) {
+    errors.categories = "Enter a category";
+  }
+  if (!values.content) {
+    errors.content = "Enter some content";
+  }
+
+  // If errors is empty, the form is fine to submit
+  // If error has any properties, redux form assumes form is invalid
+  return errors;
+}
+
 // helper allowing redux form to communicate from component to the reducer
 export default reduxForm({
+  validate,
   form: 'PostsNewForm'
 })(PostsNew);
 
 // string assigned to form property must be unique
+// validate: validate
